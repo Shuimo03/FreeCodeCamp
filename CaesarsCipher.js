368ab7305ce319f3460ccfd894fb18629dbe70f2
Cola <wu1998102@gmail.com>
2018-07-11 08:39:08 +0800

    Update CaesarsCipher.js

--- CaesarsCipher.js
+++ CaesarsCipher.js
@@ -29,16 +29,24 @@
 // alert(text);
 
 function rot13(str){
-    var result = "";
-    for(var i = 0; i<str.length; i++){
-        var currenCode = str[i].charCodeAt();
-        switch(currenCode.toUpperCase())
-        {
-            case currenCode<78:result+=String.fromCharCode(currenCode+13);break;
-            case currenCode>78:result+=String.fromCharCode(currenCode-13);break;
-        }
+    var result = ""; //保留字符串
+    for(var i = 0 ;i<str.length;i++){
+            var currenCode = str[i].charCodeAt();
+            if(currenCode>90 || currenCode<65)
+            {
+                result+=String.fromCharCode(currenCode);
+            }
+            else if(currenCode<78)
+            {
+                result+=String.fromCharCode(currenCode+13);
+            }
+            else
+            {
+                result+=String.fromCharCode(currenCode-13);
+            }
     }
     return result;
 }
+
 var text = rot13("serr pbqr pnzc");
 alert(text);
